bb71416f34725d3378853be28476022aa9592f1e
Andy <andytruong94@gmail.com>
2017-11-01 18:43:41 +0100

    done lab1 no extension

--- EDAN01/src/lab1/Photo.java
+++ EDAN01/src/lab1/Photo.java
@@ -1,8 +1,24 @@
 package lab1;
 
+import java.util.Arrays;
+
+import org.jacop.constraints.Alldifferent;
+import org.jacop.constraints.Distance;
+import org.jacop.constraints.Eq;
+import org.jacop.constraints.IfThenElse;
+import org.jacop.constraints.SumInt;
+import org.jacop.constraints.XeqC;
+import org.jacop.core.IntVar;
+import org.jacop.core.Store;
+import org.jacop.search.DepthFirstSearch;
+import org.jacop.search.IndomainMin;
+import org.jacop.search.Search;
+import org.jacop.search.SelectChoicePoint;
+import org.jacop.search.SimpleSelect;
+
 public class Photo {
 	public static void main(String[] args) {
-		new Photo().example(1);
+		new Photo().example(3);
 	}
 
 	private void example(int i) {
@@ -29,10 +45,52 @@ public class Photo {
 					{ 8, 14 }, { 9, 13 }, { 10, 11 } };
 			solve(n3, n_prefs3, prefs3);
 			break;
+		default:
+			System.out.println("No such example!");
 		}
 	}
 
 	private void solve(int n, int n_prefs, int[][] prefs) {
-		// TODO Auto-generated method stub
+		// Create store
+		Store store = new Store();
+
+		// People placed at index 1 to n
+		IntVar[] people = new IntVar[n];
+		for (int i = 0; i < n; i++) {
+			people[i] = new IntVar(store, "x" + (i + 1), 1, n);
+		}
+		store.impose(new Alldifferent(people));
+
+		// The preferred distance
+		Distance[] dist_prefs = new Distance[n_prefs];
+		for (int i = 0; i < n_prefs; i++) {
+			dist_prefs[i] = new Distance(people[prefs[i][0] - 1], people[prefs[i][1] - 1], new IntVar(store, 1, 1));
+		}
+
+		// The cost vars
+		IntVar[] pref = new IntVar[n_prefs];
+		for (int i = 0; i < n_prefs; i++) {
+			pref[i] = new IntVar(store, -1, 0);
+		}
+
+		// Add constraint if preferred distance is fulfilled the var is -1 otherwise 0
+		for (int i = 0; i < n_prefs; i++) {
+			store.impose(new IfThenElse(dist_prefs[i], new XeqC(pref[i], -1), new XeqC(pref[i], 0)));
+		}
+
+		// Sum the cost and minimize
+		IntVar cost = new IntVar(store, "cost", -n_prefs, 0);
+		store.impose(new SumInt(store, pref, "==", cost));
+
+		Search<IntVar> search = new DepthFirstSearch<IntVar>();
+		SelectChoicePoint<IntVar> select = new SimpleSelect<IntVar>(people, null, new IndomainMin<IntVar>());
+		boolean result = search.labeling(store, select, cost);
+		if (result) {
+			System.out.println("\n*** Yes");
+			System.out.println("Solution: " + Arrays.asList(people));
+			System.out.println("Cost: " + (-1) * cost.value());
+		} else {
+			System.out.println("\n*** No");
+		}
 	}
 }
